9514fd7ed190cff87c7b0f12080182c2b8134caa
Chris Jantzen <jantzeca@mail.uc.edu>
2018-10-28 22:27:47 -0400

    fixed bug in functions

--- task1/HW8_1_Task1_functions_jantzeca.py
+++ task1/HW8_1_Task1_functions_jantzeca.py
@@ -1,11 +1,13 @@
 from HW8_1_Task1_jantzeca import parallelResistance, seriesResistance
 
 print('Enter 2 resistance values to be calculated:')
-firstResistance = int(input('First value: '))
-secondResistance = int(input('Second value: '))
+firstResistance = float(input('First value: '))
+secondResistance = float(input('Second value: '))
+
+print(firstResistance, secondResistance)
 
 parallelTest = parallelResistance(firstResistance, secondResistance)
-seriesTest = seriesResistance(secondResistance, secondResistance)
+seriesTest = seriesResistance(firstResistance, secondResistance)
 
 print('Parallel network type: ', parallelTest)
 print('Series network type: ', seriesTest)
