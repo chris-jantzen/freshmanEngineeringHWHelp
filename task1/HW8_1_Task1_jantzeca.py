45502d0cfb504c10c1bd7fb2ba4babdfa5c69418
Chris Jantzen <jantzeca@mail.uc.edu>
2018-10-28 20:04:48 -0400

    updated comment

--- task1/HW8_1_Task1_jantzeca.py
+++ task1/HW8_1_Task1_jantzeca.py
@@ -8,6 +8,11 @@ def seriesResistance(firstRes, secondRes):
   rTotal = firstRes + secondRes
   return rTotal
 
+
+# How to make these one line, but a little less readable
+# so I wouldn't use these in your homework, but it's worth
+# seeing anyway for understanding how you really should be
+# thinking when writing code. (More the parallel one than the other)
 def howParallelShouldLook(firstRes, secondRes):
   return (firstRes**-1 + secondRes**-1)**-1
 
