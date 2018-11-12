1ca8e2cc41a98b4c14696bbc6357653bf287eb70
Chris Jantzen <jantzeca@mail.uc.edu>
2018-11-11 19:09:04 -0500

    start hw2

---
+++ hw2/Homework_11_2_Task1_jantzeca.py
@@ -0,0 +1,10 @@
+
+def checkValidMultipleInputs(list):
+  if len(list) != 2:
+    return False
+  
+
+# Get indecies of refraction
+indeciesOfRefraction = input('Enter indices of refraction for bottom two media:\n').split()
+angleOfIncidence = input('Enter angle of incidence (in degrees):\n')
+distances = input('Enter d1 and d2 (units):\n')
