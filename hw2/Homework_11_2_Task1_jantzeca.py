77ead9d49c1e4000d98a58f2eaa59eb7dc3c9434
Chris Jantzen <jantzeca@mail.uc.edu>
2018-11-11 22:35:36 -0500

    task 1

--- hw2/Homework_11_2_Task1_jantzeca.py
+++ hw2/Homework_11_2_Task1_jantzeca.py
@@ -1,10 +1,36 @@
+import math
 
-def checkValidMultipleInputs(list):
-  if len(list) != 2:
-    return False
-  
-
-# Get indecies of refraction
 indeciesOfRefraction = input('Enter indices of refraction for bottom two media:\n').split()
-angleOfIncidence = input('Enter angle of incidence (in degrees):\n')
-distances = input('Enter d1 and d2 (units):\n')
+if len(indeciesOfRefraction) != 2:
+  print('Need two indecies of refraction')
+elif indeciesOfRefraction[0] > indeciesOfRefraction[1]:
+  print('Error, no refraction in the second media')
+  exit()
+
+for i in range(len(indeciesOfRefraction)):
+  indeciesOfRefraction[i] = float(indeciesOfRefraction[i])
+
+angleOfIncidence = float(input('Enter angle of incidence (in degrees):\n'))
+
+distances = input('Enter d1 and d2 (units):\n').split()
+if len(indeciesOfRefraction) != 2:
+  print('Need two distances')
+
+for i in range(len(distances)):
+  distances[i] = float(distances[i])
+
+# n1 sin1(angle1) = n2 sin2(angle2)
+def getTheta2(n1, n2, theta1):
+  return math.degrees(math.radians(90) - math.asin((n1 * math.sin(math.radians(theta1))/n2)))
+
+def getBottomLength(d, theta):
+  return d * math.tan(math.radians(theta))
+
+theta2 = getTheta2(indeciesOfRefraction[0], indeciesOfRefraction[1], angleOfIncidence)
+print('theta2: {}'.format(theta2))
+length1 = getBottomLength(distances[0], angleOfIncidence)
+print('length1: {}'.format(length1))
+length2 = getBottomLength(distances[1], theta2)
+print('length2: {}'.format(length2))
+d3 = length1 + length2
+print(d3)
