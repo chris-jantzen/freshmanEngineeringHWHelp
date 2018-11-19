0abbd13482ddeb007c715df01477bb1f63f0ac60
Chris Jantzen <jantzeca@mail.uc.edu>
2018-11-18 22:32:35 -0500

    problem three

---
+++ hw3/HW_12_2_P3_jantzeca.py
@@ -0,0 +1,26 @@
+from random import randint
+
+# P(Win) = 2/6 = 1/3
+# P(Loss) = 4/6 = 2/3
+
+roll = -1
+
+numRolls = [10, 100, 1000, 10000, 100000, 1000000]
+wins = [0, 0, 0, 0, 0, 0]
+index = 0
+for num in numRolls:
+  for i in range(num):
+    roll = randint(1, 6)
+    if roll >= 5:
+      wins[index] += 1
+  index += 1
+
+index = 0
+for num in numRolls:
+  print('For', num, 'Rolls')
+  numWins = wins[index]
+  print('Percent Wins:', (numWins/num)*100)
+  print('Percent Losses:', ((num-numWins)/num) * 100)
+  index += 1
+    
+  
